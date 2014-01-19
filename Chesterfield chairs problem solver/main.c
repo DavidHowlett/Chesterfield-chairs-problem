e038e95af808891817618221dd9d03e39bfc7daf
David Howlett <David@android-d8a4bdf64956bda.gateway.2wire.net>
2014-01-19 15:26:50 +0000

    Initial Commit

---
+++ Chesterfield chairs problem solver/main.c
@@ -0,0 +1,18 @@
+//
+//  main.c
+//  Chesterfield chairs problem solver
+//
+//  Created by David Howlett on 19/01/2014.
+//  Copyright (c) 2014 David Howlett. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[])
+{
+
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
+
